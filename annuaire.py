39c433a6566680f48f07e1678f67d6e69c209be1
KirrimK <KirrimK@users.noreply.github.com>
2020-12-09 17:10:34 +0100

    Ajout de ActioCapteur, amélioration de la documentation et tests de couverture

--- annuaire.py
+++ annuaire.py
@@ -4,28 +4,71 @@ import time
 
 class Equipement:
     """Définit un équipement branché sur le robot
+
+    Attributs:
     - nom (str): le nom de l'équipement
-    - last_updt (float): timestamp de la derniere update de la valeur"""
+    - unite (str): unite dans laquelle les valeurs sont exprimées (non utilisé ici)
+    - last_updt (float): timestamp de la derniere mise à jour de la valeur"""
     def __init__(self, nom):
         self.nom = nom
         self.last_updt = time.time()
+        self.unite = None
 
     def updt(self):
-        """Met à jour le timestamp de la dernière update"""
+        """Met à jour le timestamp de la dernière mise à jour"""
         self.last_updt = time.time()
+
+    def set_state(self, valeur):
+        """Non utilisé ici, mais implémenté pour robustesse
+
+        Entrée:
+        - valeur (inutilisé)
+
+        Sortie:
+        - None
+        """
+        return None
+
+    def get_state(self):
+        """Non utilisé ici, mais implémenté pour robustesse
+
+        Sortie:
+        - None
+        """
+        return None
+
+    def get_unit(self):
+        """Retourne l'unité de l'équipement
+
+        Sortie:
+        - unite (str)
+        """
+        return self.unite
+
+    def get_last_updt(self):
+        """Retourne le timestamp de la dernière mise à jour
+
+        Sortie:
+        - last_updt (float)
+        """
+        return self.last_updt
 
 class Actionneur(Equipement):
     """Définit un actionneur basique attaché à un robot,
     pouvant prendre une valeur (int) entre max et min (int), le tout dans une certaine unité
 
+    Attributs:
+    Hérités d'Equipement
     - nom (str): le nom de l'actionneur
-    - valeur (int): l'état actuel de l'actionneur (compris entre min et max)
-    - min (int): valeur minimale prise par l'actionneur
-    - max (int): valeur max prise par l'actionneur
     - unite (str): l'unité dans laquelle les valeurs de l'actionneurs sont exprimées
     - last_updt (float): timestamp de la derniere update de la valeur
+
+    Nouveaux:
+    - valeur (int): valeur imposée à l'actionneur (compris entre min et max)
+    - min (int): valeur minimale prise par l'actionneur
+    - max (int): valeur max prise par l'actionneur
     """
-    def __init__(self, nom, valeur, min_val, max_val, unite):
+    def __init__(self, nom, valeur, min_val, max_val, unite=None):
         super().__init__(nom)
         self.valeur = valeur
         self.min_val = min_val
@@ -41,50 +84,127 @@ class Actionneur(Equipement):
         return "Actionneur [{}] Val.: {} ({}) entre {} et {}".format(nom, val, unite, min_v, max_v)
 
     def get_state(self):
-        """Retourne la valeur et le min/max d'un actionneur"""
+        """Retourne la valeur et le min/max d'un actionneur
+
+        Sortie:
+        - state (int, int, int)
+            [0]: valeur de l'actionneur
+            [1]: valeur min
+            [2]: valeur max
+        """
         return (self.valeur, self.min_val, self.max_val)
 
     def set_state(self, valeur):
-        """Change la valeur de l'actionneur"""
+        """Change la valeur de l'actionneur
+
+        Entrée:
+        - valeur (int) (devrait être compris entre min et max actionneur inclus)
+        """
         if self.min_val <= valeur <= self.max_val:
             self.valeur = valeur
             self.updt()
 
-    def get_unit(self):
-        """Renvoie l'unité de l'actionneur"""
-        return self.unite
-
 class Capteur(Equipement):
     """Définit un capteur avec un nom, une valeur et une unité
+
+    Attributs:
+    Hérités d'Equipement
     - nom (str): le nom du capteur
-    - valeur (int): valeur du capteur
     - unite (str): unité du capteur
-    - last_updt (float): timestamp de la derniere update de la valeur"""
-    def __init__(self, nom, valeur, unite):
+    - last_updt (float): timestamp de la derniere update de la valeur
+
+    Nouveaux:
+    - valeur (int): valeur du capteur"""
+    def __init__(self, nom, valeur, unite=None):
         super().__init__(nom)
         self.valeur = valeur
         self.unite = unite
 
-    def get_unit(self):
-        """Renvoie l'unité du capteur"""
-        return self.unite
-
     def get_state(self):
-        """Renvoie la valeur du capteur"""
+        """Renvoie la valeur du capteur
+
+        Sortie:
+        - valeur (int)"""
         return self.valeur
 
     def set_state(self, valeur):
-        """Change la valeur du capteur"""
+        """Change la valeur du capteur
+
+        Entrée:
+        - valeur (int)"""
         self.valeur = valeur
         self.updt()
 
+class ActioCapteur(Equipement):
+    """Définit un actionneur basique couplé à un capteur, possédant la même unité,
+    et le capteur mesurant une valeur supposément régie par l'actionneur
+
+    Attributs:
+    Hérités d'Equipement
+    - nom (str): le nom de l'ensemble
+    - unite (str): l'unité commune (remplace les unités des actionneurs)
+    - last_updt (float): timestamp de la derniere update de la valeur
+
+    Nouveaux:
+    - actionneur (Equipement): l'actionneur à relier (pas besoin de préciser l'unité)
+    - capteur (Equipement): le capteur à relier (pas besoin de préciser l'unité)
+    """
+    def __init__(self, nom, actionneur, capteur, unite_cmn):
+        super().__init__(nom)
+        self.actionneur = actionneur
+        self.capteur = capteur
+        self.unite = unite_cmn
+
+    def set_state(self, valeur):
+        """Met à jour la valeur de l'un des composants
+
+        Entrée:
+        - valeur (int, int)
+            [0]: valeur de l'actionneur (None pour ne pas changer)
+            [1]: valeur du capteur (None pour ne pas changer)
+        """
+        if valeur[0] is not None:
+            self.actionneur.set_state(valeur[0])
+            self.capteur.updt()
+        if valeur[1] is not None:
+            self.capteur.set_state(valeur[1])
+            self.capteur.updt()
+
+    def get_state(self):
+        """Retourne la valeur de l'actionneur, son min, max et la valeur du capteur
+
+        Sortie:
+        - state (int, int, int, int)
+            [0]: valeur de l'actionneur
+            [1]: min actionneur
+            [2]: max actionneur
+            [3]: valeur capteur
+        """
+        a_vl, a_mn, a_mx = self.actionneur.get_state()
+        return (a_vl, a_mn, a_mx, self.capteur.get_state())
+
+    def get_last_updt(self):
+        """Retourne les dernières actualisations de l'actionneur et du capteur
+
+        Sortie:
+        - (float, float)
+            [0]: last_updt actionneur
+            [1]: last_updt capteur
+        """
+        return (self.actionneur.last_updt, self.actionneur.last_updt)
+
 class Robot:
     """Classe définissant un robot avec les attributs suivants:
+
+    Attributs:
     - id (str): le nom du robot
     - x (float): la position selon l'axe x du robot sur la carte (en mm)
     - y (float): la position selon l'axe x du robot sur la carte (en mm)
     - theta (float): l'orientation du robot (en radians/sens trigo depuis axe x)
-    - modules (list of Equipement): liste des équipements connectés au robot
+    - equipements (dict): liste des équipements connectés au robot
+        clé: nom (str)
+        valeur: equipement (Equipement)
+        (passer les equipements lors de la construction sous forme de list)
     """
     def __init__(self, rid, x=1500, y=1000, theta=0, equipements=None):
         self.rid = rid
@@ -109,56 +229,115 @@ class Robot:
         return repr_str
 
     def get_pos(self):
-        """Récupère la position du robot sous la forme
-        (x, y, theta)"""
+        """Récupère la position du robot
+
+        Sortie:
+        - (float, float, float):
+            [0]: x
+            [1]: y
+            [2]: theta
+        """
         return (self.x, self.y, self.theta)
 
     def set_pos(self, x, y, theta):
-        """Met à jour la position (x, y, theta) du robot"""
+        """Met à jour la position du robot
+
+        Entrée:
+        - x (float)
+        - y (float)
+        - theta (float)
+        """
         self.x = x
         self.y = y
         self.theta = theta
         self.last_updt_pos = time.time()
 
     def check_eqp(self, eqp_name):
-        """Verifie si un équipement est rattaché au robot"""
+        """Verifie si un équipement est rattaché au robot
+
+        Sortie:
+        - bool
+        """
         return eqp_name in self.get_all_eqp()
 
     def updt_eqp(self, equipement):
-        """Ajoute/met à jour un actionneur du robot"""
+        """Ajoute/met à jour un actionneur du robot
+
+        Entrée:
+        - equipement (Equipement)
+        """
         self.equipements[equipement.nom] = equipement
 
     def remove_eqp(self, eqp_name):
-        """Enlève un équipement repéré par son nom du robot"""
+        """Enlève un équipement repéré par son nom du robot
+
+        Entrée:
+        - eqp_name (str)"""
         self.equipements.pop(eqp_name, None)
 
     def get_all_eqp(self):
-        """Retourne la liste de tous les équipements sur le robot"""
+        """Retourne la liste de tous les équipements sur le robot
+
+        Sortie:
+        - list of (str): liste des noms des équipements"""
         return list(self.equipements.keys())
 
     def get_state_eqp(self, eqp_name):
-        """Retourne la valeur d'un équipement (et autres informations le cas échéant)"""
-        if self.check_eqp(eqp_name) and self.get_type_eqp(eqp_name) is not Equipement:
-            return self.equipements[eqp_name].get_state()
+        """Retourne la valeur d'un équipement (et autres informations le cas échéant)
+
+        Sortie: dépend du type de l'équipement portant le nom eqp_name
+        > Equipement:
+            - None
+        > Capteur:
+            - int (Se référer à Capteur.get_state())
+        > Actionneur:
+            - (int, int, int) (Se référer à Actionneur.get_state())
+        > ActioCapteur:
+            - (int, int, int, int) (Se référer à ActioCapteur.get_state())
+        """
+        return self.equipements[eqp_name].get_state()
 
     def set_state_eqp(self, eqp_name, valeur):
-        """Change la valeur d'un équipement"""
-        if self.check_eqp(eqp_name) and self.get_type_eqp(eqp_name) is not Equipement:
+        """Change la valeur d'un équipement
+
+        Entrée: dépend du type de l'équipement portant le nom eqp_name
+        > Equipement:
+            - non utilisé, passer None
+        > Capteur:
+            - int (Se référer à Capteur.set_state()
+        > Actionneur:
+            - int (Se référer à Actionneur.set_state()
+        > ActioCapteur:
+            - (int, int) (Se référer à ActioCapteur.set_state())
+        """
+        if self.check_eqp(eqp_name):
             self.equipements[eqp_name].set_state(valeur)
 
     def get_type_eqp(self, eqp_name):
-        """Retourne le type d'un équipement"""
+        """Retourne le type d'un équipement
+
+        Sortie:
+        - type (Type)"""
         if self.check_eqp(eqp_name):
             return type(self.equipements[eqp_name])
+        return None
 
     def get_unit_eqp(self, eqp_name):
-        """Retourne l'unité d'un équipement"""
-        if self.check_eqp(eqp_name) and self.get_type_eqp(eqp_name) is not Equipement:
-            return self.equipements[eqp_name].get_unit()
+        """Retourne l'unité d'un équipement
+
+        Sortie:
+        - unite (str)"""
+        return self.equipements[eqp_name].get_unit()
 
 class Annuaire:
     """Classe définissant un espace ou toutes les informations sur les robots trackés
-    sont centralisées"""
+    sont centralisées
+
+    Attributs:
+    - robots (dict): dictionnaire contenant tous les objets Robot gardés en mémoire
+        clé: nom (str)
+        valeur: robot (Robot)
+    """
     def __init__(self):
         self.robots = {}
 
@@ -169,63 +348,143 @@ class Annuaire:
         return repr_str
 
     def check_robot(self, rid):
-        """Vérifie si un robot est dans l'annuaire"""
+        """Vérifie si un robot est dans l'annuaire
+
+        Sortie:
+        - bool"""
         return rid in self.robots
 
     def add_robot(self, robot):
-        """Ajoute un robot à l'annuaire"""
+        """Ajoute un robot à l'annuaire
+
+        Entrée:
+        - robot (Robot)"""
         self.robots[robot.rid] = robot
 
-    def remove_robot(self, robot):
-        """Enlève un robot de l'annuaire"""
-        self.robots.pop(robot.rid, None)
+    def remove_robot(self, rid):
+        """Enlève un robot de l'annuaire
+
+        Entrée:
+        - rid (str)
+        """
+        self.robots.pop(rid, None)
 
     def get_robot_pos(self, rid):
-        """Récupère la position d'un robot dans l'annuaire"""
+        """Récupère la position d'un robot stocké dans l'annuaire
+
+        Entrée:
+        - rid (str)
+
+        Sortie:
+        - (float, float, float)
+            [0]: x
+            [1]: y
+            [2]: theta
+        """
         if self.check_robot(rid):
             return self.robots[rid].get_pos()
 
     def set_robot_pos(self, rid, x, y, theta):
-        """Met à jour la position d'un robot dans l'annuaire"""
+        """Met à jour la position d'un robot dans l'annuaire
+
+        Entrée:
+        - rid (str)
+        - x (float)
+        - y (float)
+        - theta (float)
+        """
         if self.check_robot(rid):
             self.robots[rid].set_pos(x, y, theta)
 
     def updt_robot_eqp(self, rid, equipement):
         """Ajoute/met à jour la disposition d'un équipement
-        sur un robot présent dans l'annuaire (ne pas utiliser pour mettre la valeur à jour)"""
+        sur un robot présent dans l'annuaire (ne pas utiliser pour mettre la valeur à jour)
+
+        Entrée:
+        - rid (str)
+        - equipement (Equipement)
+        """
         if self.check_robot(rid):
             self.robots[rid].updt_eqp(equipement)
 
     def remove_robot_eqp(self, rid, eqp_name):
-        """Enlève un équipement repéré par son nom d'un robot présent dans l'annuaire"""
+        """Enlève un équipement repéré par son nom d'un robot présent dans l'annuaire
+
+        Entrée:
+        - rid (str)
+        - eqp_name (str)
+        """
         if self.check_robot(rid):
             self.robots[rid].remove_eqp(eqp_name)
 
     def get_robot_eqp_state(self, rid, eqp_name):
         """Retourne l'état et les possibles d'un équipement
-        monté sur un robot dans l'annuaire"""
+        monté sur un robot dans l'annuaire
+
+        Entrée:
+        - rid (str)
+        - eqp_name (str)
+
+        Sortie:
+            (Se référer à Robot.get_state_eqp())"""
         if self.check_robot(rid):
             return self.robots[rid].get_state_eqp(eqp_name)
 
     def set_robot_eqp_state(self, rid, eqp_name, valeur):
-        """Change l'état d'un équipement monté sur un robot"""
+        """Change l'état d'un équipement monté sur un robot
+
+        Entrée:
+        - rid (str)
+        - eqp_name (str)
+        - valeur (Se référer à Robot.set_state_eqp())
+        """
         if self.check_robot(rid):
             self.robots[rid].set_state_eqp(eqp_name, valeur)
 
     def get_robot_eqp_type(self, rid, eqp_name):
-        """Retourne la variété d'un équipement monté sur un robot"""
+        """Retourne la variété d'un équipement monté sur un robot
+
+        Entrée:
+        - rid (str)
+        - eqp_name (str)
+
+        Sortie:
+        - (Se référer à Robot.get_type_eqp())
+        """
         if self.check_robot(rid):
             return self.robots[rid].get_type_eqp(eqp_name)
 
     def get_robot_eqp_unit(self, rid, eqp_name):
-        """Retourne l'unité d'un équipement monté sur un robot"""
+        """Retourne l'unité d'un équipement monté sur un robot
+
+        Entrée:
+        - rid (str)
+        - eqp_name (str)
+
+        Sortie:
+        - (Se référer à Robot.get_unit_eqp())
+        """
         if self.check_robot(rid):
             return self.robots[rid].get_unit_eqp(eqp_name)
 
     def get_robot_all_eqp(self, rid):
-        """Retourne la liste de tous les équipements d'un robot"""
+        """Retourne la liste de tous les équipements d'un robot
+
+        Entrée:
+        - rid (str)
+
+        Sortie:
+        - (Se réferer à Robot.get_all_eqp())
+        """
         return self.robots[rid].get_all_eqp()
 
     def get_all_robots(self):
-        """Retourne la liste de tous les robotids des robots présents dans l'annuaire"""
+        """Retourne la liste de tous les robotids des robots présents dans l'annuaire
+
+        Sortie:
+        - list of (str)
+        """
         return list(self.robots.keys())
+
+#TODO: Faire les tests de toutes les fonctions nouvellement ajoutées
+#TODO: Rajouter les méthodes qui manquent à la suite des tests
