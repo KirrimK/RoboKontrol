cba3ea6cbe868c6e2e488793c5dcd839be0c34cc
KirrimK <KirrimK@users.noreply.github.com>
2021-01-05 18:22:09 +0100

    Commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # origine mise en bas à gauche x vers la droite, y vers le haut, theta= 0 dans axe x sens trigo
 # la radio a besoin de 0.5secondes pour se lancer.
 #TODO: SerialRadio interchangeable avec IvyRadio, backend adaptable
+#TODO: regarder lib argsparse pour arguments ligne de cmd meilleurs
 import window
 import backend as ben
 import annuaire as an
