3af905b0f98a4b9788b7ae41c97eb2184de97e55
KirrimK <KirrimK@users.noreply.github.com>
2021-01-06 14:22:22 +0100

    Ajout fichier exemple

---
+++ carte/tutorial_plagiat.py
@@ -0,0 +1,31 @@
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt5.QtGui import QPainter, QBrush, QPen
+from PyQt5.QtCore import Qt
+import sys
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = "PyQt5 Drawing Tutorial"
+        self.top= 150
+        self.left= 150
+        self.width = 500
+        self.height = 500
+        self.InitWindow()
+
+    def InitWindow(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.show()
+    
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setPen(QPen(Qt.green,  8, Qt.DashLine))
+        painter.drawEllipse(40, 40, 400, 400)
+        painter.setPen(QPen(Qt.green,  8, Qt.SolidLine))
+        painter.setBrush(QBrush(Qt.red, Qt.SolidPattern))
+
+App = QApplication(sys.argv)
+window = Window()
+sys.exit(App.exec())
