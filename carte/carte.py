de590fe8e87c336b5c0550ff7ddab2508acf057e
unknown <jacquesserien@gmail.com>
2021-01-06 15:21:19 +0100

    divers

--- carte/carte.py
+++ carte/carte.py
@@ -50,15 +50,23 @@ class RobotItem(QGraphicsEllipseItem):
         self.robot = r
         self.simulation = simu
         # build the ellipse
-        width = #5
+        width = 5
         self.setRect(-width, -width, width * 2, width * 2)
         #todo # add tooltip
         tooltip = r.type.name + ' ' + f.call_sign + ' ' + f.qfu
         self.setToolTip(tooltip)
 
-    def mousePressEvent(self, event):
-        """control du robot en cliquant sur la carte"""
+    def controlerobot(self, event):
+        """controle du robot en cliquant sur la carte et avec le clavier"""
         # Do nothing for the moment...
+            shortcut = QtWidgets.QShortcut(QtGui.QKeySequence(text), self)
+            shortcut.activated.connect(slot)
+
+        #add_shortcut('b_up', lambda: ))
+        #add_shortcut('b_down', lambda:)
+        #add_shortcut('b_left', lambda )
+        #add_shortcut('b_right', lambda )
+        #return toolbar
         event.accept()
 
     def update_position(self):
