3661a1f33d29ba8eb06dbe39c30471e3f30809f5
unknown <hippolyte.foulongne@gmail.com>
2021-01-18 17:41:32 +0100

    légères modifs

--- tab_robot.py
+++ tab_robot.py
@@ -128,6 +128,7 @@ class TabRobot(QWidget):
         self.QLineEdit_positionCommand.setInputMask("0000 : 0000 : 000")
         self.QLineEdit_positionCommand.setFixedSize(220, 25)
         self.QLineEdit_positionCommand.editingFinished.connect(self.onEditingFinished)
+        self.QLineEdit_positionCommand.setAlignment(QT_CENTER)
         self.layout_last_command.addWidget(self.QLineEdit_positionCommand)
         self.layout_box_robot.addLayout(self.layout_last_command)
 
