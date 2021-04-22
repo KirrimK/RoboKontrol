1d7a6924901459bfae496b4585925c8f94f309bd
Azarbi <victor755431@gmail.com>
2021-04-22 15:25:18 +0200

    Clarification pour que VsCode ne s'ennerve pas

--- src/main.py
+++ src/main.py
@@ -4,12 +4,12 @@ import window
 import sys
 import backend as ben
 if len (sys.argv) > 1 and sys.argv [1] in ["ivy", "Ivy"]:
-    import ivy_radio as rd
+    import ivy_radio as rdi
     print("Mode Ivy démarré.")
 else:
     print("Mode Série démarré.")
-    import serial_radio as rd
+    import serial_radio as rds
 
 if __name__ == "__main__":
-    with ben.Backend(rd.Radio() if sys.argv[1] in ["ivy", "Ivy"] else rd.Radio(sys.argv[1])) as backend:
+    with ben.Backend(rdi.Radio() if sys.argv[1] in ["ivy", "Ivy"] else rds.Radio(sys.argv[1])) as backend:
         window.main(backend)
